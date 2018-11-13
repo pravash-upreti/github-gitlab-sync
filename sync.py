70099acc73d7db852fdfcd0216e9c4c6aeace745
pravash <pravashupreti@lftechnology.com>
2018-11-13 19:55:18 +0545

    first clear and track

--- sync.py
+++ sync.py
@@ -1,9 +1,9 @@
 from subprocess import call
 
 def sync():
-    
-    call("./track_repo.sh",shell=True)
-    
+        
     call("./clear_local_branch.sh",shell=True)
+
+    call("./track_repo.sh",shell=True)
 
     call("./code_sync.sh",shell=True)
