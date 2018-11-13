943bea7ce78dcf4bc55e37e1376b93c66678ccb8
pravash <pravashupreti@lftechnology.com>
2018-11-13 20:49:53 +0545

    changed origin

--- sync.py
+++ sync.py
@@ -2,8 +2,8 @@ from subprocess import call
 
 def sync():
         
-    call("./clear_local_branch.sh",shell=True)
-
     call("./track_repo.sh",shell=True)
+
+    call("./clear_local_branch.sh",shell=True)
 
     call("./code_sync.sh",shell=True)
